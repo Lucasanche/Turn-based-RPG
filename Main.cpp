4ef8f05bf0b165fca0d9e7e4ac98aa24ef034d31
Lucasanche <lucasg.sanchez94@gmail.com>
2022-05-29 15:56:51 -0300

    base del juego con SFML configurado

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,22 @@
-int main() {
-    hola giles
-	return 1;
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Proyectazo");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return 0;
 }
