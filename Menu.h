87aead2efe956f7ca17dc4f47dae4874561527be
GiuliB94 <giuliananboggio@gmail.com>
2022-06-01 22:36:23 -0300

    corregido push

---
+++ Menu.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+#define MAX_NUMBER_OF_ITEMS 3
+
+class Menu
+{
+public:
+	Menu(float width, float height);
+	~Menu();
+
+	void draw(sf::RenderWindow& window);
+	void MoveUp();
+	void MoveDown();
+	int GetPressedItem() { return selectedItemIndex; }
+
+private:
+	int selectedItemIndex;
+	sf::Font font;
+	sf::Text menu[MAX_NUMBER_OF_ITEMS];
+
+};
