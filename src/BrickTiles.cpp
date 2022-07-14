22ffd7ef94b155bb1c05e607e2ab0ff3856270f7
Julieta <julieta.corbacho@gmail.com>
2022-07-14 01:15:11 -0300

    Sprite vortex

--- src/BrickTiles.cpp
+++ src/BrickTiles.cpp
@@ -5,11 +5,23 @@
 BrickTiles::BrickTiles() {
 	_texture.loadFromFile("./Textures/Backgrounds/Tilesheet.png");
 	_sprite.setTexture(_texture);
+	_frameVortex = 1;
 }
 
 void BrickTiles::update(int j, int i, int x, int y) {
 	_sprite.setPosition(j * 35, i * 35);
-	_sprite.setTextureRect(sf::IntRect(x * 35, y * 35, 35, 35));
+	if (x == 0) {
+		_sprite.setTextureRect(sf::IntRect(x * 35, y * 35, 35, 35));
+	}
+	else
+	{
+		_frameVortex += 0.15;
+		if (_frameVortex >= 10) {
+			_frameVortex = 1;
+		}
+		_sprite.setTextureRect(sf::IntRect( int(_frameVortex) * 35, 0, 35, 35));
+
+	}
 
 }
 
